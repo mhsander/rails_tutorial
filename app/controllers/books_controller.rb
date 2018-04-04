0fa6a0f9e98f2084771e838a9959229369fe3178
mhsander <marthahsander@gmail.com>
2018-04-03 18:05:03 -0700

    summary fixed, CRUD added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    puts @books.inspect
   end
 
   def show
@@ -16,6 +17,27 @@ class BooksController < ApplicationController
 
     @book.save
     redirect_to @book
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
